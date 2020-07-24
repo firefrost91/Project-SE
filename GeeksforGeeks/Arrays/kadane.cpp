a2ef46b628209bba08ea43bbf29199c68b3111ed
Anshuman Mishra <f20170622@pilani.bits-pilani.ac.in>
2020-07-24 20:20:59 +0530

    Kadane's Algo

---
+++ GeeksforGeeks/Arrays/kadane.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h> 
+using namespace std; 
+int Kadane(vector<int> v1, int n)
+{   int maxsofar= INT_MIN  , maxendinghere= 0  ; 
+    for(int i = 0 ; i < n ; i ++ ){
+    maxendinghere +=v1[i] ; 
+    if(maxendinghere < v1[i]){
+        maxendinghere=v1[i];
+    }
+    if(maxsofar < maxendinghere){
+        maxsofar = maxendinghere ; 
+    }
+    
+   }
+    return maxsofar ; 
+} 
+int main() 
+{ 
+     int t ; 
+     cin >> t ; 
+     while(t--){
+         int n ; 
+         cin >> n ; 
+         vector<int> v1 ; 
+         for (int i = 0 ; i < n ; i ++) {
+             int num ; 
+             cin >> num ; 
+             v1.push_back(num); 
+         }
+       
+       int maxsofar =  Kadane(v1 , n )  ; 
+        cout << maxsofar << endl ; 
+} }
